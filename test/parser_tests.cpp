361225fbc75dcd50bd4c293f763ce046a1e573f5
ryanstrace <ryanstrace@outlook.com>
2024-06-17 09:23:08 -0500

    added tests for G03_IJK_Arcs

--- test/parser_tests.cpp
+++ test/parser_tests.cpp
@@ -123,7 +123,7 @@ TEST(ParseTests, G01) {
     EXPECT_TRUE(rtmc_is_equal(parsed_block.coefficients[RTMC_Z_AXIS][3], 12.5));
 }
 
-TEST(ParseTests, G02_IJK_Arcs_Invalid) {
+TEST(ParseTests, G02_IJK_Arcs_Syntax) {
     rtmc_parsed_block_t parsed_block;
     double start_coords[RTMC_NUM_AXES] = {0};
 
@@ -158,6 +158,72 @@ TEST(ParseTests, G02_IJK_Arcs_Coefficients) {
 
     double A = sqrt(2e4);
     double B = -acos(-3e4 / sqrt(1e9));
+    double C_y = (3*RTMC_PI) / (4*B);
+    double C_x = RTMC_PI / (4*B);
+    double D_x = 0;
+    double D_y = 50;
+    EXPECT_TRUE(rtmc_is_equal(parsed_block.coefficients[RTMC_X_AXIS][0], A));
+    EXPECT_TRUE(rtmc_is_equal(parsed_block.coefficients[RTMC_X_AXIS][1], B));
+    EXPECT_TRUE(rtmc_is_equal(parsed_block.coefficients[RTMC_X_AXIS][2], C_x));
+    EXPECT_TRUE(rtmc_is_equal(parsed_block.coefficients[RTMC_X_AXIS][3], D_x));
+    EXPECT_TRUE(rtmc_is_equal(parsed_block.coefficients[RTMC_Y_AXIS][0], A));
+    EXPECT_TRUE(rtmc_is_equal(parsed_block.coefficients[RTMC_Y_AXIS][1], B));
+    EXPECT_TRUE(rtmc_is_equal(parsed_block.coefficients[RTMC_Y_AXIS][2], C_y));
+    EXPECT_TRUE(rtmc_is_equal(parsed_block.coefficients[RTMC_Y_AXIS][3], D_y));
+
+    double true_end_pos[12] = {0};
+    true_end_pos[RTMC_X_AXIS] = A*sin(B*(1-C_x)) + D_x;
+    true_end_pos[RTMC_Y_AXIS] = A*sin(B*(1-C_y)) + D_y;
+
+    double target_position[12] = {0};
+    target_position[RTMC_X_AXIS] = 100;
+    target_position[RTMC_Y_AXIS] = 250;
+
+    double position_error[12] = {0};
+    position_error[RTMC_X_AXIS] = true_end_pos[RTMC_X_AXIS] - target_position[RTMC_X_AXIS];
+    position_error[RTMC_Y_AXIS] = true_end_pos[RTMC_Y_AXIS] - target_position[RTMC_Y_AXIS];
+
+    // actually check that the position error is correct
+    for(int i = 2; i < RTMC_NUM_AXES; i++) {
+        EXPECT_TRUE(rtmc_is_equal(position_error[i], parsed_block.position_error[i]));
+    }
+}
+
+TEST(ParseTests, G03_IJK_Arcs_Syntax) {
+    rtmc_parsed_block_t parsed_block;
+    double start_coords[RTMC_NUM_AXES] = {0};
+
+    // valid - just setting modal data (not a path though!)
+    parsed_block = rtmc_parse("G03", start_coords);
+    EXPECT_TRUE(parsed_block.is_valid);
+    EXPECT_FALSE(parsed_block.is_path);
+
+    // invalid - no plane is selected
+    parsed_block = rtmc_parse("G03 X100 Y250 I100 J100", start_coords);
+    EXPECT_FALSE(parsed_block.is_valid);
+
+    // invalid - no feed rate set
+    parsed_block = rtmc_parse("G17 G03 X100 Y250 I100 J100", start_coords);
+    EXPECT_FALSE(parsed_block.is_valid);
+
+    // invalid - missing an offset (I and J words)
+    parsed_block = rtmc_parse("G17 G03 F100 X100 Y250", start_coords);
+    EXPECT_FALSE(parsed_block.is_valid);
+}
+
+TEST(ParseTests, G03_IJK_Arcs_Coefficients) {
+    rtmc_parsed_block_t parsed_block;
+    double start_coords[RTMC_NUM_AXES] = {0};
+    
+    start_coords[0] = -100;
+    start_coords[1] = -50;
+    parsed_block = rtmc_parse("G17 G03 F100 X100 Y250 I100 J100", start_coords);
+    EXPECT_TRUE(parsed_block.is_valid);
+    EXPECT_TRUE(parsed_block.is_path);
+    EXPECT_EQ(parsed_block.path_type, RTMC_TRIGONOMETRIC);
+
+    double A = sqrt(2e4);
+    double B = 2*RTMC_PI - acos(-3e4 / sqrt(1e9));
     double C_y = (3*RTMC_PI) / (4*B);
     double C_x = RTMC_PI / (4*B);
     double D_x = 0;
